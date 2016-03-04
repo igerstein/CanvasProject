899cfc77e2cdbce8d52043f4677746bba9db04b1
igerstein <isaac.gerstein1@gmail.com>
2016-03-04 12:31:46 -0500

    Started drawing grid on canvas

---
+++ connect.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.beginPath();
+ctx.moveTo(70, 0);
+ctx.lineTo(70, 420);
+ctx.stroke();
