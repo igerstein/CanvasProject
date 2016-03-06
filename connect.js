98f595f101dbbf9a4917487ae0415815847d5392
igerstein <isaac.gerstein1@gmail.com>
2016-03-06 08:54:27 -0500

    Tokens are represented in an array and stack properly

--- connect.js
+++ connect.js
@@ -1,6 +1,16 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+//Stores the position of the tokens(0 = empty, 1 = red, 2 = yellow)
+var tokens = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
 //Draws a line from a start point to an end point
 var drawLine = function(startX, startY, endX, endY){
     ctx.strokeStyle = "black";
@@ -30,6 +40,16 @@ var drawGrid = function(){
 
 drawGrid();
 
+//Adds the token to the token array in the given column and returns the row
+var addToArray = function(col){
+    for (var i = 0; i < tokens.length; i++){
+	if (i == tokens.length - 1 || tokens[i + 1][col] != 0){
+	    tokens[i][col] = 1;
+	    return i;
+	}
+    }
+};
+
 //Draws a token at the top of a column
 var drawToken = function(e){
     var x = Math.floor(e.clientX / 70) * 70 + 35;
@@ -47,6 +67,8 @@ var fall = function(e){
     var x = Math.floor(e.clientX / 70) * 70 + 35;
     var y = 35;
 
+    var finalY = addToArray(Math.floor(x / 70)) * 70 + 105;
+    
     var fallAnimation = function(){
 	
 	ctx.clearRect(x - 35, 0, 70, y + 35);
@@ -58,7 +80,7 @@ var fall = function(e){
 	ctx.arc(x, y, 35, 0, Math.PI * 2);
 	ctx.fill();
 
-	if (y < 455){
+	if (y < finalY){
 	    y += 10;
 	    window.requestAnimationFrame(fallAnimation);
 	};
